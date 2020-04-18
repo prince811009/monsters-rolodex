775df85e1d18b28ed91f67d37e762cb27573010c
prince811009 <prince811009fy@gmail.com>
2020-04-18 13:04:22 +0800

    Updates

---
+++ precache-manifest.67558159e6de74bd7559e4dc847cc9ec.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4bba18b817f9ce309f17d56ff03452be",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "825dae7e6a1fe41028ac",
+    "url": "/monsters-rolodex/static/css/main.235af448.chunk.css"
+  },
+  {
+    "revision": "02020da58069e4d76d13",
+    "url": "/monsters-rolodex/static/js/2.febbfa51.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.febbfa51.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "825dae7e6a1fe41028ac",
+    "url": "/monsters-rolodex/static/js/main.9501855a.chunk.js"
+  },
+  {
+    "revision": "942cc55f18445d5e4474",
+    "url": "/monsters-rolodex/static/js/runtime-main.ac5f337d.js"
+  }
+]);
